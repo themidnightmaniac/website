b13d219dce26f28a9b699618e621368e4a86f8de
themidnightmaniac <inscientebberman@tutanota.com>
2024-02-13 00:23:49 -0300

    Added Comments on the code/Layout Improv./Fixed Some Errors/Art Gallery now has Themes

--- gallery.js
+++ gallery.js
@@ -1,4 +1,4 @@
-/*Check For Previously Selected  Theme*/
+/*Check For Selected Theme*/
 
 var theme = document.getElementsByTagName('link')[0];
 
@@ -14,37 +14,37 @@ function savetheme(){
 
   if (prevtheme == 'main') {
 
-    theme.setAttribute('href', 'themes/main.css');
+    theme.setAttribute('href', '/gallery/themes/main.css');
     document.getElementById("theme-button").innerHTML = "Midnight";
 
   }else if (prevtheme == 'midnight') {
 
-    theme.setAttribute('href', 'themes/midnight.css');
+    theme.setAttribute('href', '/gallery/themes/midnight.css');
     document.getElementById("theme-button").innerHTML = "Neo";
 
   }else if (prevtheme == 'neo') {
   
-    theme.setAttribute('href', 'themes/neo.css');
+    theme.setAttribute('href', '/gallery/themes/neo.css');
     document.getElementById("theme-button").innerHTML = "Heaven";
   
   }else if (prevtheme == 'heaven') {
   
-    theme.setAttribute('href', 'themes/heaven.css');
+    theme.setAttribute('href', '/gallery/themes/heaven.css');
     document.getElementById("theme-button").innerHTML = "Cancer";
   
   }else if (prevtheme == 'cancer') {
   
-    theme.setAttribute('href', 'themes/cancer.css');
+    theme.setAttribute('href', '/gallery/themes/cancer.css');
     document.getElementById("theme-button").innerHTML = "Wild";
   
   }else if (prevtheme == 'wild') {
   
-    theme.setAttribute('href', 'themes/wild.css');
+    theme.setAttribute('href', '/gallery/themes/wild.css');
     document.getElementById("theme-button").innerHTML = "Main";
   
   }else {
   
-    theme.setAttribute('href', 'themes/main.css')
+    theme.setAttribute('href', '/gallery/themes/main.css')
     document.getElementById("theme-button").innerHTML = "Midnight";
   
   }
