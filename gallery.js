c09a862cd47fe73f1cf9f54a0cf29f9f1517344c
themidnightmaniac <inscientebberman@tutanota.com>
2024-02-13 01:03:55 -0300

    Layout Adjustments to werk on 4:3

--- gallery.js
+++ gallery.js
@@ -15,37 +15,30 @@ function savetheme(){
   if (prevtheme == 'main') {
 
     theme.setAttribute('href', '/gallery/themes/main.css');
-    document.getElementById("theme-button").innerHTML = "Midnight";
 
   }else if (prevtheme == 'midnight') {
 
     theme.setAttribute('href', '/gallery/themes/midnight.css');
-    document.getElementById("theme-button").innerHTML = "Neo";
 
   }else if (prevtheme == 'neo') {
   
     theme.setAttribute('href', '/gallery/themes/neo.css');
-    document.getElementById("theme-button").innerHTML = "Heaven";
   
   }else if (prevtheme == 'heaven') {
   
     theme.setAttribute('href', '/gallery/themes/heaven.css');
-    document.getElementById("theme-button").innerHTML = "Cancer";
   
   }else if (prevtheme == 'cancer') {
   
     theme.setAttribute('href', '/gallery/themes/cancer.css');
-    document.getElementById("theme-button").innerHTML = "Wild";
   
   }else if (prevtheme == 'wild') {
   
     theme.setAttribute('href', '/gallery/themes/wild.css');
-    document.getElementById("theme-button").innerHTML = "Main";
   
   }else {
   
     theme.setAttribute('href', '/gallery/themes/main.css')
-    document.getElementById("theme-button").innerHTML = "Midnight";
   
   }
 }
